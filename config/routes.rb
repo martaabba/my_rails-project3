d2b2bbac2042aae66aa96e24b87bb5c48acc518d
martaabba <danika.stochosky@gmail.com>
2016-03-12 16:46:12 +0000

    routes not functioning-no app running here

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  
+ 
+  GET 'static_pages/about'
+
+  GET 'static_pages/contact'
+
+  GET 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
