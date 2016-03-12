442692bb8543b6df3688c57222e991964c88f19b
martaabba <danika.stochosky@gmail.com>
2016-03-12 16:53:43 +0000

    reconfigured route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   
  
-  GET 'static_pages/about'
+  get 'static_pages/about'
 
-  GET 'static_pages/contact'
+  get 'static_pages/contact'
 
-  GET 'static_pages/index'
+  get 'static_pages/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
