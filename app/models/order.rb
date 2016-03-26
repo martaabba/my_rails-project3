5350c9867dba147964fd83fac7b5ed463459882c
martaabba <danika.stochosky@gmail.com>
2016-03-26 03:28:33 +0000

    product and order association created

---
+++ app/models/order.rb
@@ -0,0 +1,3 @@
+class Order <ActiveRecord::Base
+    belongs_to :product #order model belongs_to Product Model--links foreign key or individual order no
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ActiveRecord::Base
+    has_many :orders #Product model has_many orders--because many orders have the same product on them
 end
