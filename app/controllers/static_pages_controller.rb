9c890d6e25965b2e2232caabb7f8da0ab72781dc
martaabba <danika.stochosky@gmail.com>
2016-04-16 01:20:33 +0000

    message correction

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
-  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body').deliver_now  
+  
   
   
   
