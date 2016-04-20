1c91657e3bddeb1a794dca862ae08d5047e63486
martaabba <danika.stochosky@gmail.com>
2016-04-20 00:04:04 +0000

    email address action mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,7 +14,7 @@ class StaticPagesController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(:from => @email,
-      :to => 'your-email@example.com',
+      :to => 'danika.stochosky@gmail.com',
       :subject => "A new contact form message from #{@name}", 
       :body => @message).deliver_now
   end
