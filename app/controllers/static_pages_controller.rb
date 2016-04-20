501f66e8d9458b4b2b1cb5d9977faec72ad0e15d
martaabba <danika.stochosky@gmail.com>
2016-04-20 04:39:41 +0000

    test text on static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,19 +8,13 @@ class StaticPagesController < ApplicationController
   def thank_you
     @name = params[:name]
     @email = params[:email]
-    @message = params[:message]
+    @message = "test text" + params[:message]
     ActionMailer::Base.mail(:from => @email,
       :to => 'danika.stochosky@gmail.com',
       :subject => "A new contact form message from #{@name}", 
       :body => @message).deliver_now
   end
  
-   def thank_you
-        @name = params[:name]
-        @email = params[:email]
-        @message = params[:message]
-        UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
 
 
 
