13539ad48d59d43b189b223c8149bd9f237d52f0
martaabba <danika.stochosky@gmail.com>
2016-04-20 04:58:34 +0000

    thankyou only defined in static pages

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,12 +8,12 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
     end
     
-    def thank_you
-        @name = params[:name]
-        @email = params[:email]
-        @message = params[:message]
-        UserMailer.contact_form(@email, @name, @message).deliver_now
-    end
+    #def thank_you
+      #  @name = params[:name]
+      #  @email = params[:email]
+     #   @message = params[:message]
+     #   UserMailer.contact_form(@email, @name, @message).deliver_now
+  #  end
     
   
 end
