7787860126398056602e7671acaf937057b2f3ff
martaabba <danika.stochosky@gmail.com>
2016-04-20 04:15:44 +0000

    thankyou defined in user_mailer and in staticpagescontroller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,5 +8,10 @@ class UserMailer < ApplicationMailer
         :subject => "A new contact form message from #{name}")
     end
     
-   
+    def thank_you
+        @name = params[:name]
+        @email = params[:email]
+        @message = params[:message]
+        UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
 end
