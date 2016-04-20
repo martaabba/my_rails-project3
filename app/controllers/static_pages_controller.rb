a26eebd95c8e9481a1635f298e2453bc75d51710
martaabba <danika.stochosky@gmail.com>
2016-04-20 04:11:08 +0000

    two thankyous on static controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,16 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+def thank_you 
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+    :to => 'danika.stochosky@gmail.com',
+    :subject => "A new contact form message from #{@name}",
+    :body => @message).deliver_now
+end
+
 
   
     def thank_you
