2a36f12823e6fb0a52596b8af6bb02bc4d61355f
martaabba <danika.stochosky@gmail.com>
2016-04-20 13:54:19 +0000

    thank_you defined in both static and user_mailer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,13 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
-        @name = params[:name]
-        @email = params[:email]
-        @message = params[:message]
-        UserMailer.contact_form(@email, @name, @message).deliver_now
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'danika.stochosky@gmail.com',
+      :subject => "A new contact form message from #{@name}", 
+      :body => @message).deliver_now
   end
  
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,7 +9,12 @@ class UserMailer < ApplicationMailer
     end
     
     
-    
+    def thank_you
+       @name = params[:name]
+       @email = params[:email]
+       @message = params[:message]
+       UserMailer.contact_form(@email, @name, @message).deliver_now
+    end
     
   
 end
