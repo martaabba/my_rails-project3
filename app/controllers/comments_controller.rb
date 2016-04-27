75ed28e8ed9a8495ec9d76238fc4e75f3603caa8
martaabba <danika.stochosky@gmail.com>
2016-04-27 01:26:58 +0000

    add create to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
     def create
+        @product = Product.find(params[:product_id])
+        @comment = @product.comment.new(comment_params)
+        @comment.user = current_user
+        @comment.save
+        redirect_to_product_path(@product)
     end
     
     def destroy
