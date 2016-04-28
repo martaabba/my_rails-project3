633d7b61b14c6a9f40a72438efded92a90dbec62
martaabba <danika.stochosky@gmail.com>
2016-04-28 04:25:25 +0000

    half-way to stars

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,13 @@
+//this file contains site-wide js functions
+
+$(document).on('ready page:load', function(){
+  $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' 
+    });//close .rating.raty
+  $('rated').raty({ path: '/assets',
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });//close rated.raty
+  
+}); //close document.on for product ratings
