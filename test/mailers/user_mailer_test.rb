23495246157217bea16765cf3b01405e6c0065bd
martaabba <danika.stochosky@gmail.com>
2016-05-12 03:48:02 +0000

    review mailer

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def contact_form
+    UserMailer.contact_form("john@example.com", "John", "Hello World!")
+  end  
 end
