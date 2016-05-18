0c28da72ad1fc37818a5e6d7aba7012c6b041bd0
martaabba <danika.stochosky@gmail.com>
2016-05-18 03:44:46 +0000

    factory_girl

---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,19 @@
+FactoryGirl.define.do
+    sequence(:email) { |n| "user#{n}@example.com" } 
+    
+    factory :user do
+        email "peter@example.com"
+        password "123456789"
+        first_name "Peter"
+        last_name "Doe"
+        admin false
+    end
+    
+    factory :admin, class: User do
+        email "peter2@example.com"
+        password "qwertyuiop"
+        admin "true"
+        first_name "Admin"
+        last_name "User"
+    end
+end
