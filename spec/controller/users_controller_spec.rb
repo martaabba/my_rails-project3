42f5e9759e155ebe4f5a1726d5a373670d184371
martaabba <danika.stochosky@gmail.com>
2016-06-06 18:52:21 +0000

    factory_girl users controller a&b users deleted

--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
        
        
        before do
-        @userA = FactoryGirl.create(:user)
-        @userB = FactoryGirl.create(:user)
+        @user = FactoryGirl.create(:user)
+        
        end
        
        describe "Get #show" do
           
           context "User is logged in" do
             before do
-                sign_in(@userA)
+                sign_in(@user)
             end
             it "Loads correct user details" do
-                get :show, id: @userA.id
+                get :show, id: @user.id
                 expect(response).to have_http_status(200)
-                expect(assigns(:user)).to eq (@userA)
+                expect(assigns(:user)).to eq (@user)
             end
                 
           context "No user is logged in" do
                 it "redirects to login" do
-                  get :show, id: @userA.id
+                  get :show, id: @user.id
                   expect(response).to redirect_to(root_path)
                 end
                     
@@ -30,18 +30,17 @@ require 'rails_helper'
         
         describe "Unauthorized access" do
             before do
-                sign_in(@userA)
+                sign_in(@user)
             end
             
-            context "Attempt to access show page of userB" do
+            context "Attempt to access show page of user" do
                 it "redirects to login" do
-                    get :show, id: @userB.id
+                    get :show, id: @user.id
                     expect(response).to have_http_status(401)
                     expect(response).to redirect_to(root_path)
                 end
             end
-          
+        end
     end
             
-    
-    
+end
