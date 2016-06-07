cbf4986fc82536cde645d26639d96e61b6419062
martaabba <danika.stochosky@gmail.com>
2016-06-07 03:16:42 +0000

    payments controller stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,20 @@
 class PaymentsController < ApplicationController
+  # Set your secret key: remember to change this to your live secret key in production
+# See your keys here https://dashboard.stripe.com/account/apikeys
+    Stripe.api_key = "sk_test_j6AnHijPg1fLVP1A3QVicW1Y"
+
+# Get the credit card details submitted by the form
+    token = params[:stripeToken]
+
+# Create the charge on Stripe's servers - this will charge the user's card
+        begin
+            charge = Stripe::Charge.create(
+                :amount => 1000, # amount in cents, again
+                :currency => "usd",
+                :source => token,
+                :description => "Example charge"
+                )
+        rescue Stripe::CardError => e
+        # The card has been declined
+        end
 end
