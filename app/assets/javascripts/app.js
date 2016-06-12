3e5c13f38405acb406e109d5991557d027b8000e
martaabba <danika.stochosky@gmail.com>
2016-06-12 22:17:06 +0000

    angular services

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,19 +1,24 @@
 var app= angular.module('shop', []);
 
+app.factory('models', [function(){
+    var x = {
+        orders: []
+    };
+    return x;
+}]);
+
 $(document).on('ready page:load', function(){
     angular.bootstrap(document.body, ['shop'])
 });
 
-app.controller('OrdersCtrl', ['$scope', function($scope){
+app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
     //Here will be all code belonging to the controller 
-   $scope.orders=[
-       {id: 1, total: 24, product_id: 6, user_id: 1},
-       {id: 2, total: 7, product_id: 6},
-       {id: 3, total: 42, product_id: 6, user_id: 1}
-       ];
+    $scope.orders = model.orders;
+    
     $scope.addOrder = function(){
         if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }    
         $scope.orders.push($scope.new.Order);
+        
     $scope.deleteOrder = function(order){
         $scope.orders.splice($scope.orders.indexOf(order), 1);
         };
