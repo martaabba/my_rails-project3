f6fcb05487502749bd720404e92e03b1c63c4fd7
martaabba <danika.stochosky@gmail.com>
2016-07-03 00:01:17 +0000

    ANGULAR done

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,51 @@
-var app = angular.module('shop', []);
+var app = angular.module('shop', ['ngResource']);
 
 $(document).on('ready page:load', function(){
 	angular.bootstrap(document.body, ['shop'])
 });
 
-app.controller('OrdersCtrl', ['$scope', function($scope){
+app.factory('models', ['$resource', function($resource){
+    var orders_model = $resource("/orders/:id.json", {id: "@id"});
+    var products_model = $resource("/products/:id.json", {id: "@id"});
+    
+    var x = {
+        orders: orders_model,
+        products: products_model
+    };
+    return x;
+}]);
+
+app.controller('OrdersCtrl', ['$scope', '$models', function($scope, models){
     //here will be all the code belonging to this controller
-    $scope.orders =[
-        {id: 1, total: 24, product_id: 4, user_id: 1},
-        {id: 2, total: 7, product_id: 4},
-        {id: 3, total: 42, product_id: 4, user_id: 1}
-    ];
-    $scope.addOrder = function(){
-        if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){return; }
-        $scope.orders.push($scope.newOrder);
-    }
+   $scope.orders = models.orders.query();
+   $scope.orders = models.products.query();
+    //$scope.orders =[
+   //     {id: 1, total: 24, product_id: 4, user_id: 1},
+     //   {id: 2, total: 7, product_id: 4},
+       // {id: 3, total: 42, product_id: 4, user_id: 1}
+    //];
     
-    $scope.deleteOrder = function(order){
-        $scope.orders.splice($scope.orders.indexOf(order), 1);
-    }
+    app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
+
+	$scope.orders = models.orders.query();
+	$scope.products = models.products.query();
+
+	$scope.addOrder = function(){
+		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
+		order = models.orders.save($scope.newOrder, function(){
+			recent_order = models.orders.get({id: order.id});
+			$scope.orders.push(recent_order);
+			$scope.newOrder = '';
+		});
+
+	}
+
+	$scope.deleteOrder = function(order){
+		models.orders.delete(order);
+		$scope.orders.splice($scope.orders.indexOf(order), 1)
+	}
+
     
     
     
-}]);//end OrdersCtrl
+}]);
