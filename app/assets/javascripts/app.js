aa688df8212aed79b8d77f167d286c69cf16e4d3
martaabba <danika.stochosky@gmail.com>
2016-07-06 18:18:07 +0000

    Adjust controller and factory in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,7 +7,6 @@ $(document).on('ready page:load', function(){
 app.factory('models', ['$resource', function($resource){
     var orders_model = $resource("/orders/:id.json", {id: "@id"});
     var products_model = $resource("/products/:id.json", {id: "@id"});
-    
     var x = {
         orders: orders_model,
         products: products_model
@@ -15,37 +14,26 @@ app.factory('models', ['$resource', function($resource){
     return x;
 }]);
 
-app.controller('OrdersCtrl', ['$scope', '$models', function($scope, models){
-    //here will be all the code belonging to this controller
-   $scope.orders = models.orders.query();
-   $scope.orders = models.products.query();
-    //$scope.orders =[
-   //     {id: 1, total: 24, product_id: 4, user_id: 1},
-     //   {id: 2, total: 7, product_id: 4},
-       // {id: 3, total: 42, product_id: 4, user_id: 1}
-    //];
+
+   
     
-    app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
+app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
 
-	$scope.orders = models.orders.query();
-	$scope.products = models.products.query();
+	    $scope.orders = models.orders.query();
+    	$scope.products = models.products.query();
 
-	$scope.addOrder = function(){
-		if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
-		order = models.orders.save($scope.newOrder, function(){
+    	$scope.addOrder = function(){
+	    	if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
+		    order = models.orders.save($scope.newOrder, function(){
 			recent_order = models.orders.get({id: order.id});
 			$scope.orders.push(recent_order);
 			$scope.newOrder = '';
 		});
 
-	}
+	};
 
 	$scope.deleteOrder = function(order){
 		models.orders.delete(order);
-		$scope.orders.splice($scope.orders.indexOf(order), 1)
-	}
-
-    
-    
-    
+		$scope.orders.splice($scope.orders.indexOf(order), 1);
+	};
 }]);
